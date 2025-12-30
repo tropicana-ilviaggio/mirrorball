4c29e2c696ecf047d901089aa4fc102ab84f2b73
tropicana-ilviaggio <alessandra.s.cappelletti@gmail.com>
2025-12-30 18:59:37 +0100

    Remove audio and text entries from daily
    Removed an audio entry and a text entry from dailyContent.

--- daily.js
+++ daily.js
@@ -1,7 +1,5 @@
 const dailyContent = [
     { type: "text", data: "Oggi è un giorno misterioso..." },
-    { type: "audio", data: "audio/esempio.mp3" },
-    { type: "text", data: "Questo è un nuovo testo di prova!" },
     { type: "audio", data: "audio/prova.mp3" },
     { type: "text", data: "Se io volessi scrivere qualcosa di molto molto molto molto molto molto lungo... se io volessi... che cosa accadrebbe?" },
 ];
