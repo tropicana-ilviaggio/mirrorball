af6c9dcc18b255ac48d67da04d60b110da8e1a7c
tropicana-ilviaggio <alessandra.s.cappelletti@gmail.com>
2025-12-30 18:41:05 +0100

    Aggiunto testo e audio di prova

--- daily.js
+++ daily.js
@@ -1,4 +1,6 @@
 const dailyContent = [
     { type: "text", data: "Oggi è un giorno misterioso..." },
     { type: "audio", data: "audio/esempio.mp3" }
+     { type: "text", data: "Questo è un nuovo testo di prova!" },
+    { type: "audio", data: "audio/prova.mp3" }
 ];
